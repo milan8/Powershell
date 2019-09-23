aa0b9aa5b01b45c48d961fd69a67ba1242068688
milan8 <milan_antony@yahoo.co.in>
2019-09-23 18:36:35 +0530

    RandomNumberGenerator

---
+++ GenerateRandomNumber.ps1
@@ -0,0 +1,8 @@
+﻿# Random Number - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-random?view=powershell-6
+for ($i=1;$i -le 100;$i++) {
+      $employees = @(
+      
+      $randomnumber= Get-Random -Maximum 1000
+      Write-Host "Writing line" $randomnumber
+
+      )}
